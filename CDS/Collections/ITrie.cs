d2d868b474a103ef89a45b4467947244b3cece71
alex <alexbanks097@gmail.com>
2017-10-25 18:03:53 +0200

    Add Trie Interface ITrie

---
+++ CDS/Collections/ITrie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDS.Collections
+{
+    public interface ITrie<Value>
+    {
+        Value Get(string key);
+        void Put(string key, Value val);
+    }
+}
