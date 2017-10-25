d950698330aaf0366751baa1198fd7915ac6e811
alex <alexbanks097@gmail.com>
2017-10-25 18:27:02 +0200

    Add Count get and private setter and private Get and Put methods

--- CDS/Collections/TernarySearchTrie.cs
+++ CDS/Collections/TernarySearchTrie.cs
@@ -6,12 +6,25 @@ namespace CDS.Collections
 {
     public class TernarySearchTrie<Value> : ITrie<Value>
     {
+        // Returns the number of 
+        public int Count { get; private set;  } 
         public Value Get(string key)
         {
             throw new NotImplementedException();
         }
 
+        private Node Get(Node node, string key, int depth)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Put(string key, Value val)
+        {
+            throw new NotImplementedException();
+            Count++;
+        }
+
+        private Node Put(Node node, string key, Value val, int depth)
         {
             throw new NotImplementedException();
         }
