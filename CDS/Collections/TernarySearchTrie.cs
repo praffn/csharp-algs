d57ad448773b58ebaf291f48965787098f4c56e7
alex <alexbanks097@gmail.com>
2017-10-25 18:04:24 +0200

    Add TernarySearchTrie that implements Trie

---
+++ CDS/Collections/TernarySearchTrie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDS.Collections
+{
+    public class TernarySearchTrie<Value> : ITrie<Value>
+    {
+        public Value Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Put(string key, Value val)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Node
+        {
+            char c;
+            Node left, mid, right;
+            Value val;
+        }
+    }
+}
